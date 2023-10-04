9b84d786ebbf7393be47ae916bba75a640f7128f
DjuneC <cesarrodelin@outlook.com>
2023-10-04 15:00:08 -0500

    Modify data experience

--- lib/data.ts
+++ lib/data.ts
@@ -35,28 +35,28 @@ export const links = [
 
 export const experiencesData = [
   {
-    title: "Graduated bootcamp",
-    location: "Miami, FL",
+    title: "Python/Django Developer",
+    location: "Cap-Haitian, HT",
     description:
-      "I graduated after 6 months of studying. I immediately found a job as a front-end developer.",
+      "I worked as a Django developer for 1 year.",
     icon: React.createElement(LuGraduationCap),
-    date: "2019",
+    date: "2021-2022",
+  },
+  {
+    title: "NodeJs/Backend Developer",
+    location: "Cap-Haitian, HT",
+    description:
+      "I worked as a backend developer for 1 year. I also upskilled to the full stack.",
+    icon: React.createElement(CgWorkAlt),
+    date: "2022-2023",
   },
   {
     title: "Front-End Developer",
-    location: "Orlando, FL",
+    location: "San Antonio, TX",
     description:
-      "I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.",
-    icon: React.createElement(CgWorkAlt),
-    date: "2019 - 2021",
-  },
-  {
-    title: "Full-Stack Developer",
-    location: "Houston, TX",
-    description:
-      "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
+      "I'm now a frontend developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
     icon: React.createElement(FaReact),
-    date: "2021 - present",
+    date: "2023 - present",
   },
 ] as const;
 
