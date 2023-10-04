1cf5fc2c70e5093484a2d7c5e76dd3c161e60d6c
DjuneC <cesarrodelin@outlook.com>
2023-10-04 16:27:20 -0500

    Minimal changes

--- components/intro.tsx
+++ components/intro.tsx
@@ -63,10 +63,11 @@ export default function Intro() {
         animate={{ opacity: 1, y: 0 }}
       >
         <span className="font-bold">Hello, I'm Junior.</span> I'm a{" "}
-        <span className="font-bold">full-stack developer</span> with{" "}
+        <span className="font-bold">Backend (full-stack) developer</span> with{" "}
         <span className="font-bold">2+ years</span> of experience. I enjoy
-        building <span className="italic">sites & apps</span>. My focus is{" "}
-        <span className="underline">MERN and CS</span>.
+        building <span className="italic">sites & apps</span>. 
+        {/* My focus is{" "}
+        <span className="underline">MERN and CS</span>. */}
       </motion.h1>
 
       <motion.div
